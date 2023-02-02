191141a95d3fb37be338538577789bcc2ae63689
Jesse <jesse.velasquez40@gmail.com>
2023-02-01 22:14:28 -0800

    round number tracker and 12 round limit

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ let bScore = document.querySelector('#bot_score')
 let resultArea = document.querySelector('#result')
 let playerImg = document.querySelector('#player_img')
 let botImg = document.querySelector('#bot_img')
+let roundNum = document.querySelector('#round')
+let count = 0
 
 let playerSelection;
 
@@ -37,28 +39,41 @@ function playRound(computerSelection, x) {
 
     if ((computerSelection === 'rock' && playerSelection.toLowerCase() === 'paper') || (computerSelection === 'paper' && playerSelection.toLowerCase() === 'scissors') || (computerSelection === 'scissors' && playerSelection.toLowerCase() === 'rock')) {
         playerImg.src = `./images/${playerSelection}.png`
-        botImg.src = `./images/${computerSelection}.png`
+        botImg.src = `./images/${computerSelection}bot.png`
         pScore.textContent = `${playerScore += 1}`
         bScore.textContent = `${computerScore}`
-        resultArea.textContent = 'You win!'
+        resultArea.textContent = 'You won the round!'
+        count++
+        roundNum.textContent = `Round ${count} of 12`
     } else if ((computerSelection === 'rock' && playerSelection.toLowerCase() === 'scissors') || (computerSelection === 'paper' && playerSelection.toLowerCase() === 'rock') || (computerSelection === 'scissors' && playerSelection.toLowerCase() === 'paper')) {
         playerImg.src = `./images/${playerSelection}.png`
-        botImg.src = `./images/${computerSelection}.png`
+        botImg.src = `./images/${computerSelection}bot.png`
         pScore.textContent = `${playerScore}`
         bScore.textContent = `${computerScore += 1}`
-        resultArea.textContent = 'Computer wins!'
+        resultArea.textContent = 'Computer won the round!'
+        count++
+        roundNum.textContent = `Round ${count} of 12`
     } else if ((computerSelection === 'rock' && playerSelection.toLowerCase() === 'rock') || (computerSelection === 'paper' && playerSelection.toLowerCase() === 'paper') || (computerSelection === 'scissors' && playerSelection.toLowerCase() === 'scissors')) {
         playerImg.src = `./images/${playerSelection}.png`
-        botImg.src = `./images/${computerSelection}.png`
+        botImg.src = `./images/${computerSelection}bot.png`
         pScore.textContent = `${playerScore}`
         bScore.textContent = `${computerScore}`
-        resultArea.textContent = 'Draw!'
-    } 
+        resultArea.textContent = 'Round is a draw!'
+        count++
+        roundNum.textContent = `Round ${count} of 12`
+    }
+    
+    if (count === 11) {
+        resultArea.textContent = 'Last Round!'
+    } else if (count === 12) {
+        endGame()
+        restart()
+    }
+
 } 
 
 function game() {
-
-console.log(playRound())
+    console.log(playRound())
 if (playerScore > computerScore) {
     return 'You are the winner!'
 } else if (computerScore > playerScore) {
@@ -68,3 +83,12 @@ if (playerScore > computerScore) {
 }
 }
 
+function endGame() {
+    btns.forEach(btn => {
+        btn.classList.add('remove')
+    })
+}
+
+function restart() {
+
+}
